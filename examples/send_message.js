4ff5ff5d064ab2aa3e653fb9b3141b56b52763c6
Astro <astro@spaceboyz.net>
2010-05-26 04:19:18 +0200

    examples/send_message.js & various fixes

---
+++ examples/send_message.js
@@ -0,0 +1,26 @@
+require.paths.push('../lib');
+var sys = require('sys');
+var xmpp = require('xmpp');
+var argv = process.argv;
+
+if (argv.length < 6) {
+    sys.puts('Usage: node send_message.js <my-jid> <my-password> <my-text> <jid1> [jid2] ... [jidN]');
+    process.exit(1);
+}
+
+var cl = new xmpp.Client({ jid: argv[2],
+			   password: argv[3],
+			   host: 'jabber.ccc.de',
+			   port: 5222 });
+cl.addListener('online',
+	       function() {
+		   argv.slice(5).forEach(
+		       function(to) {
+			   cl.send(new xmpp.XML.Element('message',
+							{ to: to,
+							  type: 'chat'}).
+				   c('body').
+				   t(argv[4]));
+		       });
+		   cl.end();
+	       });
