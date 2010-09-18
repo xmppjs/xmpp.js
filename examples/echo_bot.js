9c6650c2ac79ed6d80925ae1f2603943296509ad
Andreas Maierhofer <a.maierhofer@gmail.com>
2010-09-18 19:14:55 +0200

    modified client constructor function to read host and port params

--- examples/echo_bot.js
+++ examples/echo_bot.js
@@ -5,35 +5,33 @@ var sys = require('sys');
 var xmpp = require('../lib/node-xmpp');
 var argv = process.argv;
 
-if (argv.length != 4) {
-    sys.puts('Usage: node echo_bot.js <my-jid> <my-password>');
-    process.exit(1);
+if (argv.length < 4) {
+	sys.puts('Usage: node echo_bot.js <my-jid> <my-password> <my-domain if differs>');
+	process.exit(1);
 }
 
-var cl = new xmpp.Client({ jid: argv[2],
-			   password: argv[3] });
-cl.on('online',
-      function() {
-	  cl.send(new xmpp.Element('presence',
-				   { type: 'chat'}).
-		  c('show').t('chat').up().
-		  c('status').t('Happily echoing your <message/> stanzas')
-		 );
-      });
-cl.on('stanza',
-      function(stanza) {
-	  if (stanza.is('message') &&
-	      // Important: never reply to errors!
-	      stanza.attrs.type !== 'error') {
+var cl = new xmpp.Client({ jid: argv[2],password: argv[3], host: argv[4] });
 
-	      // Swap addresses...
-	      stanza.attrs.to = stanza.attrs.from;
-	      delete stanza.attrs.from;
-	      // and send back.
-	      cl.send(stanza);
-	  }
-      });
-cl.on('error',
-      function(e) {
-	  sys.puts(e);
-      });
+cl.on('online',function() {
+	sys.puts('online');
+	cl.send(new xmpp.Element('presence', { type: 'chat'}).
+	c('show').t('chat').up().
+	c('status').t('Happily echoing your <message/> stanzas'));
+});
+
+cl.on('stanza',function(stanza) {
+	if (stanza.is('message') &&
+	// Important: never reply to errors!
+	stanza.attrs.type !== 'error') {
+
+		// Swap addresses...
+		stanza.attrs.to = stanza.attrs.from;
+		delete stanza.attrs.from;
+		// and send back.
+		cl.send(stanza);
+	}
+});
+
+cl.on('error',function(e) {
+	sys.puts(e);
+});
