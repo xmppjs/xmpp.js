5f04f30f78abf8db5439981e91d430e03f2283a1
julien <julien.genestoux@gmail.com>
2011-08-19 16:20:43 -0400

    lots of changes. auth + ressource binding now works for a single user

--- examples/c2s.js
+++ examples/c2s.js
@@ -1,19 +1,16 @@
 var xmpp = require('../lib/node-xmpp');
 
-var r   = new xmpp.Router();
-var c2s = new xmpp.C2S(r, {
+var c2s = new xmpp.C2S({
     port: 5222, 
-    host: '127.0.0.1'
+    host: '127.0.0.1',
+    domain: '127.0.0.1'
 });
 
-// Called when a stanza was received from a local client.
 c2s.on("stanza", function(stanza, client) {
-    console.log(stanza);
+    // No roster support in this server!
+    if (stanza.is('iq') && stanza.attrs.type == 'get' && (session = stanza.getChild('query', 'jabber:iq:roster'))) {
+        client.send(new xmpp.Element("iq", {type:"error", id: stanza.attrs.id}).c("query", { xmlns: "jabber:iq:roster"})); 
+    }
 })
 
-// Registering stuff.
-r.register('127.0.0.1', function(stanza) {
-    console.log("<< "+stanza.toString());
-    c2s.receive(stanza); // Called when a stanza was received from 'outside'.
-});
 
