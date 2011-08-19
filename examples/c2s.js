15cd522d052d5f5f9c7fe67c879b5bc2c9677109
julien <julien.genestoux@gmail.com>
2011-08-19 09:43:29 -0400

    sasl auth not working

--- examples/c2s.js
+++ examples/c2s.js
@@ -1,8 +1,19 @@
 var xmpp = require('../lib/node-xmpp');
 
-var r = new xmpp.Router();
-var c2s = new xmpp.C2S(r);
+var r   = new xmpp.Router();
+var c2s = new xmpp.C2S(r, {
+    port: 5222, 
+    host: '127.0.0.1'
+});
+
+// Called when a stanza was received from a local client.
+c2s.on("stanza", function(stanza, client) {
+    console.log(stanza);
+})
+
+// Registering stuff.
 r.register('127.0.0.1', function(stanza) {
     console.log("<< "+stanza.toString());
-    c2s.receive(stanza);
+    c2s.receive(stanza); // Called when a stanza was received from 'outside'.
 });
+
