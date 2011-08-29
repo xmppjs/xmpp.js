b3fdf8689ac8ee90ad19b82ee13997ae4708d30e
julien <julien.genestoux@gmail.com>
2011-08-29 15:56:06 -0700

    fixing password

--- examples/debug_c2s.js
+++ examples/debug_c2s.js
@@ -1,6 +1,7 @@
 var xmpp = require('../lib/node-xmpp');
 
-var cl = new xmpp.Client({ jid: "julien@localhost", password: "hello"});
+var cl = new xmpp.Client({ jid: "julien@localhost", password: "password"});
 cl.on('online', function() {
     console.log("ONLINE!!! YIHAA");
 });
+
