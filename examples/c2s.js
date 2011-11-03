21e91a0e8324dee85d2e2e8f9b8433627dda158d
Astro <astro@spaceboyz.net>
2011-11-03 14:04:08 +0100

    c2s: don't emit events on server but on connection

--- examples/c2s.js
+++ examples/c2s.js
@@ -14,30 +14,29 @@ var c2s = new xmpp.C2SServer({
 
 });
 
-// Allows the developer to authenticate users against anything they want.
-c2s.on("authenticate", function(jid, password, client, cb) {
-    cb(true); // cb(false);
-});
-
-// Allows the developer to register the jid against anything they want
-c2s.on("register", function(jid, password, client, cb) {
-    cb(true); // cb(false, {code: "406", type: "modify", text: "not-acceptable"});
-});
-
 // On Connect event. When a client connects.
 c2s.on("connect", function(client) {
     // That's the way you add mods to a given server.
 
-    // Per-client stanza handling, alternative to c2s.on("stanza", cb)
+    // Allows the developer to register the jid against anything they want
+    c2s.on("register", function(opts, cb) {
+	cb(true);
+    });
+
+    // Allows the developer to authenticate users against anything they want.
+    client.on("authenticate", function(opts, cb) {
+	cb(null); // cb(false);
+    });
+    client.on("online", function() {
+	client.send(new xmpp.Message({ type: 'chat' }).c('body').t("Hello there, little client."));
+    });
+
+    // Stanza handling
     client.on("stanza", function(stanza) {
     });
-});
 
-// On Disconnect event. When a client disconnects
-c2s.on("disconnect", function(client) {
-});
-
-// Most imoortant pieces of code : that is where you can configure your XMPP server to support only what you care about/need.
-c2s.on("stanza", function(stanza, client) {
+    // On Disconnect event. When a client disconnects
+    client.on("disconnect", function(client) {
+    });
 
 });
