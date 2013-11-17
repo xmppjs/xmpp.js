4020b548f59c3e087044a971aeb4f124a73c3dac
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-11-17 19:02:59 +0000

    Clean up echo_server

--- examples/echo_server.js
+++ examples/echo_server.js
@@ -1,12 +1,15 @@
-var xmpp = require('../lib/node-xmpp');
+'use strict';
 
-var r = new xmpp.Router();
+var xmpp = require('../lib/node-xmpp')
+
+var r = new xmpp.Router()
+
 r.register('codetu.be', function(stanza) {
-    console.log("<< "+stanza.toString());
+    console.log('<< ' + stanza.toString())
     if (stanza.attrs.type !== 'error') {
-    var me = stanza.attrs.to;
-    stanza.attrs.to = stanza.attrs.from;
-    stanza.attrs.from = me;
-    r.send(stanza);
+        var me = stanza.attrs.to
+        stanza.attrs.to = stanza.attrs.from
+        stanza.attrs.from = me
+        r.send(stanza)
     }
-});
+})
