5b5171331f5aa3c8d5cb0b1a5872e3eb24546a34
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-11-17 18:51:34 +0000

    Cleaned up load_test

--- examples/load_test/server.js
+++ examples/load_test/server.js
@@ -1,59 +1,76 @@
-var http = require('http');
-var xmpp = require('../../lib/node-xmpp');
-var C2SStream = require('../../lib/xmpp/c2s').C2SStream;
+'use strict';
 
-var tcpClients = 0, boshClients = 0, authedClients = 0;
+var http = require('http')
+  , xmpp = require('../../lib/node-xmpp')
+  , C2SStream = require('../../lib/xmpp/c2s').C2SStream
 
-var sv = new xmpp.C2SServer({ port: 25222 });
+var tcpClients = 0,
+    boshClients = 0,
+    authedClients = 0
+
+var sv = new xmpp.C2SServer({ port: 25222 })
+
 sv.on('connect', function(svcl) {
-    tcpClients++;
+    tcpClients++
     svcl.on('close', function() {
-    tcpClients--;
-    });
+        tcpClients--
+    })
     svcl.on('authenticate', function(opts, cb) {
-    authedClients++;
-    svcl.on('close', function() {
-        authedClients--;
-    });
-    cb();
-    });
-});
+        authedClients++
+        svcl.on('close', function() {
+            authedClients--
+        })
+        cb()
+    })
+})
 
-var bosh = new xmpp.BOSHServer();
+var bosh = new xmpp.BOSHServer()
 http.createServer(function(req, res) {
     try {
-    bosh.handleHTTP(req, res);
+        bosh.handleHTTP(req, res)
     } catch(e) {
-    console.error(e.stack||e);
+        console.error(e.stack||e)
     }
-}).listen(25280);
+}).listen(25280)
 bosh.on('connect', function(svcl) {
-    boshClients++;
+    boshClients++
     svcl.on('close', function() {
-    boshClients--;
-    });
-    var c2s = new C2SStream({ connection: svcl });
+        boshClients--
+    })
+    var c2s = new C2SStream({ connection: svcl })
     c2s.on('authenticate', function(opts, cb) {
-    authedClients++;
-    svcl.on('close', function() {
-        authedClients--;
-    });
-    cb();
-    });
+        authedClients++
+        svcl.on('close', function() {
+            authedClients--
+        })
+        cb()
+    })
     c2s.on('error', function(e) {
-    console.error("Error", e);
-    });
-});
+        console.error('Error', e)
+    })
+})
 
 setInterval(function() {
-    console.log("TCP", tcpClients, "BOSH", boshClients, "authed", authedClients, "RSS MB", Math.ceil(process.memoryUsage().rss / 1024 / 1024));
+    console.log(
+        'TCP',
+        tcpClients,
+        'BOSH',
+        boshClients,
+        'authed',
+        authedClients,
+        'RSS MB',
+        Math.ceil(process.memoryUsage().rss / 1024 / 1024)
+    )
 
-    var inCount = 0, outCount = 0;
-    for(var k in bosh.sessions) {
-    var session = bosh.sessions[k];
-    for(var j in session.inQueue)
-        inCount++;
-    outCount += session.outQueue.length;
+    var inCount = 0
+      , outCount = 0
+
+    for (var k in bosh.sessions) {
+        var session = bosh.sessions[k]
+        /* jshint unused: false */
+        for (var j in session.inQueue) inCount++
+        outCount += session.outQueue.length
     }
-    console.log("BOSH inQueue", inCount, "BOSH outQueue", outCount);
-}, 1000);
+    console.log('BOSH inQueue', inCount, 'BOSH outQueue', outCount)
+
+}, 1000)
