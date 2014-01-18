11ccac3f7ae9ca6af99a88135c573c1b3b9d8648
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-01-18 20:45:05 +0000

    Add header and 'send message' code

--- examples/facebook.js
+++ examples/facebook.js
@@ -1,5 +1,20 @@
 'use strict';
 
+/**
+ * Login to facebook chat via XMPP and send a simple 'hello world' message
+ * to another user
+ *
+ * To look up a facebook user ID use: http://findmyfacebookid.com/
+ *
+ * Ensure that you obtain an access token with the extended 'xmpp_login'
+ * permission or login will fail.
+ *
+ * To obtain one of these visit: https://developers.facebook.com/tools/explorer
+ * Select to generate an access token
+ * Click 'extended permissions'
+ * Choose 'xmpp_login'
+ * Generate your access token
+ */
 var Client = require('../index')
   , ltx  = require('ltx')
 
@@ -7,6 +22,8 @@ var facebookId = '<< facebook user ID >>'
 var appId = '<< appliction ID >> '
 var appSecret = '<< application secret >>'
 var accessToken = 'Get access token from here (make sure you select your app and check extended xmpp_login permission): https://developers.facebook.com/tools/explorer'
+
+var otherUserId = '<< send message to ID >>'
 
 var client = new Client({
     jid: '-' + facebookId + '@chat.facebook.com',
@@ -18,8 +35,16 @@ var client = new Client({
 
 client.addListener('online', function(data) {
     console.log('Connected as ' + data.jid.user + '@' + data.jid.domain + '/' + data.jid.resource)
+    var chat = new ltx.Element('message', { to: '-' + otherUserId + '@chat.facebook.com' })
+        .c('body')
+        .t('Hello world')
+    client.send(chat)
     // nodejs has nothing left to do and will exit
     client.end()
+})
+
+client.on('stanza', function(stanza) {
+    console.log('Received:', stanza.toString())
 })
 
 client.addListener('error', function(e) {
