e22516907b04a21fd2bd3d6ef9fbc17bd0e30f1c
NightHawk14 <antoine.savignac@gmail.com>
2014-04-12 18:52:26 +0200

    Modifications for travis build

--- examples/prebind.js
+++ examples/prebind.js
@@ -1,15 +1,18 @@
+'use strict';
 var XMPP = require('node-xmpp-client');
 
-var client = new XMPP({
+(new XMPP({
     jid: 'me@example.com',
     password: 'secret',
     boshURL: 'http://example.com/http-bind',
     preferred: 'PLAIN',
     wait: '60',
     prebind:function(err,data){
-      /*
-        data.sid
-        data.rid
-      */
+        if(err) throw new Error(err);
+        else return data;
+        /*
+            data.sid
+            data.rid
+        */
     }
-});
+}))();
