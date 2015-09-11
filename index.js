49fa4b86502901faea0ea819d0b497a4555ae810
Sonny Piers <sonny@fastmail.net>
2015-09-11 13:05:19 +0200

    import node-xmpp-core

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 'use strict'
 
-var IQ = require('./lib/IQ')
-  , Presence = require('./lib/Presence')
-  , Message = require('./lib/Message')
-  , ltx = require('ltx')
+var JID = require('./lib/JID')
 
-var Stanza = require('./lib/Stanza')
-// deprecated, let's remove them at some point
-Stanza.Iq = IQ
-Stanza.Presence = Presence
-Stanza.Message = Message
-Stanza.Element = ltx.Element
-Stanza.Stanza = Stanza
-
-exports.Stanza = Stanza
-exports.JID = require('./lib/JID')
-exports.Connection = require('./lib/Connection')
-exports.SRV = require('./lib/SRV')
-exports.StreamParser = require('./lib/StreamParser')
-exports.ltx = ltx
-exports.Element = ltx.Element
-exports.IQ = IQ
-exports.Presence = Presence
-exports.Message = Message
+module.exports = JID
+module.exports.JID = JID
