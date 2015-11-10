8e56e06faa076f49ee5cc32780cb407996ae9fad
Sonny Piers <sonny@fastmail.net>
2015-11-10 11:47:04 +0100

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports.IQ = require('./lib/IQ')
+module.exports.Message = require('./lib/Message')
+module.exports.Presence = require('./lib/Presence')
+module.exports.Stanza = require('./lib/Stanza')
