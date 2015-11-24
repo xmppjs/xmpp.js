b248b700f3af58e7cdaf8995da9647505cadfaca
Sonny Piers <sonny@fastmail.net>
2015-11-24 12:11:10 +0100

    use node-xmpp-stanza

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 'use strict'
 
-var Stanza = require('./lib/Stanza')
-var IQ = require('./lib/IQ')
-var Presence = require('./lib/Presence')
-var Message = require('./lib/Message')
 var Connection = require('./lib/Connection')
 var StreamParser = require('./lib/StreamParser')
 
 var JID = require('node-xmpp-jid').JID
-var ltx = require('ltx')
+var stanza = require('node-xmpp-stanza')
 var inherits = require('inherits')
-
-Stanza.Iq = IQ // DEPRECATED
-Stanza.Presence = Presence // DEPRECATED
-Stanza.Message = Message // DEPRECATED
-Stanza.Element = ltx.Element // DEPRECATED
-Stanza.Stanza = Stanza // DEPRECATED
 
 exports.SRV = require('./lib/SRV')
 
@@ -23,26 +13,29 @@ exports.exportCoreUtils = function (obj) {
   // core
   obj.Connection = Connection
   obj.StreamParser = StreamParser
-  obj.Stanza = Stanza
-  obj.createStanza = Stanza.createStanza
-  obj.IQ = IQ
-  obj.Presence = Presence
-  obj.Message = Message
-
-  // ltx
-  obj.ltx = ltx
-  obj.Element = ltx.Element
-  obj.createElement = ltx.createElement
-  obj.escapeXML = ltx.escapeXML
-  obj.escapeXMLText = ltx.escapeXMLText
-  obj.Parser = ltx.Parser
-  obj.parse = ltx.parse
 
   // jid
   obj.JID = JID
 
   // inherits
   obj.inherits = inherits
+
+  // stanza
+  obj.stanza = stanza
+  obj.Stanza = stanza.Stanza
+  obj.createStanza = stanza.createStanza
+  obj.IQ = stanza.IQ
+  obj.Presence = stanza.Presence
+  obj.Message = stanza.Message
+  obj.Parser = stanza.Parser
+  obj.parse = stanza.parse
+
+  // ltx
+  obj.ltx = stanza.ltx
+  obj.createElement = stanza.createElement
+  obj.Element = stanza.Element
+  obj.escapeXML = stanza.escapeXML
+  obj.escapeXMLText = stanza.escapeXMLText
 }
 
 exports.exportCoreUtils(exports)
