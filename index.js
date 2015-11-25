220bfa91bef29e86bb4d658c9f42c542f95b690f
Sonny Piers <sonny@fastmail.net>
2015-11-25 18:07:38 +0100

    export ltx and createElement

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var ltx = require('ltx')
 module.exports.IQ = require('./lib/IQ')
 module.exports.Message = require('./lib/Message')
 module.exports.Presence = require('./lib/Presence')
+
 module.exports.Stanza = require('./lib/Stanza')
 module.exports.createStanza = require('./lib/createStanza')
+
 module.exports.parse = require('./lib/parse')
 module.exports.Parser = require('./lib/Parser')
 
 module.exports.Element = ltx.Element
+module.exports.createElement = ltx.createElement
 
 module.exports.escapeXML = ltx.escapeXML
 module.exports.escapeXMLText = ltx.escapeXMLText
@@ -19,3 +22,5 @@ module.exports.equal = ltx.equal
 module.exports.nameEqual = ltx.nameEqual
 module.exports.attrsEqual = ltx.attrsEqual
 module.exports.childrenEqual = ltx.childrenEqual
+
+module.exports.ltx = ltx
