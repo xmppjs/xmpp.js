d8545c9fef695e107cd85e7922a0b0d64fff448a
Sonny Piers <sonny@fastmail.net>
2017-02-23 18:01:09 +0100

    remove node-xmpp* packages

--- packages/node-xmpp/examples/debug_c2s.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var xmpp = require('../index')
-
-var cl = new xmpp.Client({ jid: 'julien@localhost', password: 'password' })
-
-cl.on('online', function () {
-  console.log('ONLINE!!! YIHAA')
-})
