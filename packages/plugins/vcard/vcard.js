94dbc789be257e4bc5abdb9ecf5a3fa8ff644b48
Yiorgis Gozadinos <ggozad@crypho.com>
2017-05-29 10:57:49 +0200

    Fix eslint

--- packages/plugins/vcard/vcard.js
+++ packages/plugins/vcard/vcard.js
@@ -1,54 +1,57 @@
+'use strict'
 
 const xml = require('@xmpp/xml')
 const iqCaller = require('../iq-caller')
 
 const NS_VCARD = 'vcard-temp'
 
+const parsevCard = (el) => {
+  const dict = {}
+  el.children.forEach((c) => {
+    if (c.children && typeof c.children[0] === 'string') {
+      dict[c.name] = c.text()
+    } else {
+      dict[c.name] = parsevCard(c)
+    }
+  })
+  return dict
+}
+
+const buildvCard = (dict, parent) => {
+  const builder = parent || xml`<vCard xmlns="${NS_VCARD}" version="2.0"/>`
+  for (const [key, val] of Object.entries(dict)) {
+    if (typeof val === 'object') {
+      builder.cnode(buildvCard(val, xml`<${key}/>`)).up()
+    } else if (val) {
+      builder.c(key).t(val)
+    } else {
+      builder.c(key).up()
+    }
+  }
+  return builder
+}
+
 function plugin (entity) {
   const caller = entity.plugin(iqCaller)
 
-  const _parsevCard =  (el) => {
-    let dict = {}
-    el.children.forEach((c) => {
-      if (c.children && typeof c.children[0] === 'string')
-        dict[c.name] = c.text()
-      else
-        dict[c.name] = _parsevCard(c)
-    })
-    return dict
-  }
-
-  const _buildvCard = (dict, parent) => {
-    let builder = parent ? parent : xml`<vCard xmlns="${NS_VCARD}" version="2.0"/>`
-    for (let [key, val] of Object.entries(dict)) {
-      if (typeof val === 'object')
-        builder.cnode(_buildvCard(val, xml`<${key}/>`)).up()
-      else if (val)
-        builder.c(key).t(val)
-      else
-        builder.c(key).up()
-    }
-    return builder
-  }
-
   return {
     entity,
-    get(jid) {
-      return caller.get(jid,
-        xml`<vCard xmlns='${NS_VCARD}'/>`
-      )
+    get (jid) {
+      return caller.get(jid, xml`<vCard xmlns='${NS_VCARD}'/>`)
       .then((res) => {
-        return Promise.resolve(_parsevCard(res))
+        return Promise.resolve(parsevCard(res))
       })
     },
-    set(vcard) {
-      return caller.set(null, _buildvCard(vcard))
+    set (vcard) {
+      return caller.set(null, buildvCard(vcard))
     },
   }
 }
 
-export default {
-  name: 'vCard',
+module.exports = {
+  name: 'vcard',
   NS_VCARD,
+  buildvCard,
+  parsevCard,
   plugin,
 }
