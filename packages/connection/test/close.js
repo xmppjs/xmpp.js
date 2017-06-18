2583fe15a99337406efb148d9a7102623d0307b5
Sonny Piers <sonny@fastmail.net>
2017-06-18 23:19:43 +0200

    reset properties on close and allow sasl failure without text (#427)
    reset "domain" property on close and allow sasl failure without text child

--- packages/connection/test/close.js
+++ packages/connection/test/close.js
@@ -5,6 +5,20 @@ const Connection = require('..')
 const {EventEmitter} = require('@xmpp/events')
 const xml = require('@xmpp/xml')
 
+test('resets properties on socket close event', t => {
+  const conn = new Connection()
+  conn._attachSocket(new EventEmitter())
+  conn.lang = 'en'
+  conn.jid = {}
+  conn.domain = 'example.com'
+  conn.status = 'online'
+  conn.socket.emit('close')
+  t.is(conn.lang, '')
+  t.is(conn.jid, null)
+  t.is(conn.domain, '')
+  t.is(conn.status, 'close')
+})
+
 test.cb('timeout', t => {
   t.plan(2)
   const conn = new Connection()
