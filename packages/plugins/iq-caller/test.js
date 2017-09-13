d577854123f3ecfaa7190c8035a9755adb3e532e
Sonny Piers <sonny@fastmail.net>
2017-09-13 14:12:49 +0200

    iq-caller plugin: allow passing the id

---
+++ packages/plugins/iq-caller/test.js
@@ -0,0 +1,108 @@
+'use strict'
+
+const test = require('ava')
+const iqCallerPlugin = require('./index')
+const testPlugin = require('../testPlugin')
+
+test.cb('#get', t => {
+  const {plugin, entity} = testPlugin(iqCallerPlugin)
+
+  entity.send = el => {
+    t.is(typeof el.attrs.id, 'string')
+    delete el.attrs.id
+    t.deepEqual(
+      el,
+      <iq type="get">
+        <foo />
+      </iq>
+    )
+    t.end()
+    return Promise.resolve()
+  }
+
+  plugin.get(<foo />)
+})
+
+test.cb('#set', t => {
+  const {plugin, entity} = testPlugin(iqCallerPlugin)
+
+  entity.send = el => {
+    t.is(typeof el.attrs.id, 'string')
+    delete el.attrs.id
+    t.deepEqual(
+      el,
+      <iq type="set">
+        <foo />
+      </iq>
+    )
+    t.end()
+    return Promise.resolve()
+  }
+
+  plugin.set(<foo />)
+})
+
+test.cb('#request', t => {
+  const {plugin, entity} = testPlugin(iqCallerPlugin)
+
+  entity.send = el => {
+    t.deepEqual(
+      el,
+      <iq type="get" id="foobar">
+        <foo />
+      </iq>
+    )
+    t.end()
+    return Promise.resolve()
+  }
+
+  plugin.request(
+    <iq type="get" id="foobar">
+      <foo />
+    </iq>
+  )
+})
+
+test.cb('#request with param as string', t => {
+  const {plugin, entity} = testPlugin(iqCallerPlugin)
+
+  entity.send = el => {
+    t.deepEqual(
+      el,
+      <iq type="get" id="foobar" to="service">
+        <foo />
+      </iq>
+    )
+    t.end()
+    return Promise.resolve()
+  }
+
+  plugin.request(
+    <iq type="get" id="foobar">
+      <foo />
+    </iq>,
+    'service'
+  )
+})
+
+test.cb('#request with id and to parameters', t => {
+  const {plugin, entity} = testPlugin(iqCallerPlugin)
+
+  entity.send = el => {
+    t.deepEqual(
+      el,
+      <iq type="get" id="foobar" to="service">
+        <foo />
+      </iq>
+    )
+    t.end()
+    return Promise.resolve()
+  }
+
+  plugin.request(
+    <iq type="get">
+      <foo />
+    </iq>,
+    {to: 'service', id: 'foobar'}
+  )
+})
