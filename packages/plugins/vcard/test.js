eac895cc9ee6fda484a852ddef75f6efeafd50a0
Sonny Piers <sonny@fastmail.net>
2017-09-22 12:23:06 +0200

    use new helpers for vcard plugin tests

--- packages/plugins/vcard/test.js
+++ packages/plugins/vcard/test.js
@@ -2,8 +2,8 @@
 
 const test = require('ava')
 const plugin = require('.')
-const xml = require('@xmpp/xml')
 const testPlugin = require('../testPlugin')
+const xml = require('@xmpp/xml')
 
 test.beforeEach(t => {
   t.context = testPlugin(plugin)
@@ -13,63 +13,43 @@ test('name', t => {
   t.is(plugin.name, 'vcard')
 })
 
-test.cb('get', t => {
-  t.plan(5)
+test('set', t => {
+  t.context.scheduleIncomingResult()
 
-  let id
-
-  t.context.entity.promise('send').then(stanza => {
-    id = stanza.attrs.id // eslint-disable-line prefer-destructuring
-    delete stanza.attrs.id
-    t.is(typeof id, 'string')
-
-    delete stanza.attrs.xmlns
-    t.deepEqual(stanza,
-      xml('iq',
-        {type: 'get', to: 'foo@bar'},
-        [xml('vCard', {xmlns: 'vcard-temp'})]
-      )
-    )
-
-    const reply = xml('iq', {type: 'result', id},
-      xml('vcard', {xmlns: 'vcard-temp'},
-        [xml('FN', {}, 'Foo Bar'),
-          xml('N', {}, [xml('FAMILY', {}, 'BAR'), xml('GIVEN', {}, 'Foo')]),
-        ]
-      )
-    )
-    t.context.entity.emit('element', reply)
-  })
-  t.context.plugin.get('foo@bar').then(vcard => {
-    t.is(vcard.FN, 'Foo Bar')
-    t.is(vcard.N.FAMILY, 'Bar')
-    t.is(vcard.N.GIVEN, 'Foo')
-    t.end()
-  })
-})
-
-test.cb('set', t => {
-  t.plan(2)
-
-  let id
-
-  t.context.entity.promise('send').then(stanza => {
-    id = stanza.attrs.id // eslint-disable-line prefer-destructuring
-    delete stanza.attrs.id
-    t.is(typeof id, 'string')
-
-    t.context.entity.emit('element', xml('iq', {type: 'result', id}))
-    delete stanza.attrs.xmlns
-    t.deepEqual(stanza,
-      xml('iq', {type: 'set'},
-        xml('vCard', {xmlns: 'vcard-temp', version: '2.0'}, [
+  return Promise.all([
+    t.context.catchOutgoingGet().then(child => {
+      t.deepEqual(
+        child,
+        xml('vCard', {xmlns: 'vcard-temp'}, [
           xml('FN', {}, 'Foo Bar'),
           xml('N', {}, [xml('FAMILY', {}, 'Bar'), xml('GIVEN', {}, 'Foo')]),
         ])
       )
-    )
-    t.end()
-  })
+    }),
+    t.context.plugin
+      .set({FN: 'Foo Bar', N: {FAMILY: 'Bar', GIVEN: 'Foo'}})
+      .then(value => {
+        t.deepEqual(value, undefined)
+      }),
+  ])
+})
 
-  t.context.plugin.set({FN: 'Foo Bar', N: {FAMILY: 'Bar', GIVEN: 'Foo'}})
+test('get', t => {
+  t.context.scheduleIncomingResult(
+    xml(
+      'vcard',
+      {xmlns: 'vcard-temp'},
+      xml('FN', {}, 'Foo Bar'),
+      xml('N', {}, xml('FAMILY', {}, 'Bar'), xml('GIVEN', {}, 'Foo'))
+    )
+  )
+
+  return Promise.all([
+    t.context.catchOutgoingGet().then(child => {
+      t.deepEqual(child, xml('vCard', {xmlns: 'vcard-temp'}))
+    }),
+    t.context.plugin.get().then(vcard => {
+      t.deepEqual(vcard, {FN: 'Foo Bar', N: {FAMILY: 'Bar', GIVEN: 'Foo'}})
+    }),
+  ])
 })
