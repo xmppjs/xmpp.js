0db4fb71fcdf68c51cca01f4acfe4af8b59edf0b
Yiorgis Gozadinos <ggozad@crypho.com>
2017-10-04 17:00:41 +0200

    pubsub plugin

---
+++ packages/plugins/pubsub/index.js
@@ -0,0 +1,150 @@
+'use strict'
+
+const {xml, plugin} = require('@xmpp/plugin')
+const iqCaller = require('../iq-caller')
+const stanzaRouter = require('../stanza-router')
+
+const NS_PUBSUB = 'http://jabber.org/protocol/pubsub'
+const NS_PUBSUB_EVENT = `${NS_PUBSUB}#event`
+const NS_PUBSUB_OWNER = `${NS_PUBSUB}#owner`
+const NS_PUBSUB_NODE_CONFIG = `${NS_PUBSUB}#node_config`
+const NS_ATOM = 'http://www.w3.org/2005/Atom'
+const NS_DELAY = 'urn:xmpp:delay'
+const NS_RSM = 'http://jabber.org/protocol/rsm'
+const NS_X_DATA = 'jabber:x:data'
+
+module.exports = service =>
+  plugin('pubsub', {
+    NS_PUBSUB,
+    NS_PUBSUB_EVENT,
+    NS_PUBSUB_OWNER,
+    NS_PUBSUB_NODE_CONFIG,
+    NS_ATOM,
+    NS_DELAY,
+    NS_RSM,
+    NS_X_DATA,
+
+    service,
+
+    matchPEP(stanza) {
+      const ev = stanza.getChild('event')
+      return (
+        ev &&
+        ev.attrs.xmlns === NS_PUBSUB_EVENT &&
+        stanza.is('message') &&
+        stanza.attrs.from === service
+      )
+    },
+
+    start() {
+      if (service) {
+        this.plugins['stanza-router'].add(this.matchPEP, this.onPEPEvent)
+      }
+    },
+
+    stop() {
+      if (service) {
+        this.plugins['stanza-router'].remove(this.matchPEP)
+      }
+    },
+
+    onPEPEvent(message, client) {
+      const items = message.getChild('event').getChild('items')
+      const {node} = items.attrs
+      const item = items.getChild('item')
+      const {id} = item.attrs
+      const entry = item.getChild('entry')
+      const delay = message.getChild('delay')
+
+      if (delay) {
+        const {stamp} = delay.attrs
+        client.emit('last-item-published', {node, id, entry, stamp})
+        client.emit(`last-item-published:${node}`, {id, entry, stamp})
+      } else {
+        client.emit('item-published', {node, id, entry})
+        client.emit(`item-published:${node}`, {id, entry})
+      }
+    },
+
+    createNode(node, options, ...args) {
+      const stanza = xml`
+        <pubsub xmlns='${NS_PUBSUB}'>
+          <create node='${node}'/>
+        </pubsub>`
+
+      if (options) {
+        const config = xml`<configure/>`
+        const x = config.cnode(xml`
+          <x xmlns='${NS_X_DATA}' type='submit'>
+            <field var='FORM_TYPE' type='hidden'>
+              <value>${NS_PUBSUB_NODE_CONFIG}</value>
+            </field>
+          </x>`)
+
+        for (const key of Object.keys(options)) {
+          const option = xml`
+            <field var='${key}'>
+              <value>${options[key].toString()}</value>
+            </field>`
+          x.cnode(option)
+        }
+        stanza.cnode(config)
+      }
+
+      return this.plugins['iq-caller'].set(stanza, ...args)
+      .then(result => result.getChild('create').attrs.node)
+    },
+
+    deleteNode(node, ...args) {
+      const stanza = xml`
+        <pubsub xmlns='${NS_PUBSUB}'>
+          <delete node='${node}'/>
+        </pubsub>`
+      return this.plugins['iq-caller'].set(stanza, ...args)
+    },
+
+    publish(node, item, ...args) {
+      const stanza = xml`
+        <pubsub xmlns='${NS_PUBSUB}'>
+          <publish node='${node}'></publish>
+        </pubsub>`
+      if (item) {
+        stanza.getChild('publish').cnode(item)
+      }
+      return this.plugins['iq-caller'].set(stanza, ...args)
+      .then(result => result.getChild('publish').getChild('item').attrs.id)
+    },
+
+    items(node, rsm, ...args) {
+      const stanza = xml`
+        <pubsub xmlns='${NS_PUBSUB}'>
+          <items node='${node}'/>
+        </pubsub>`
+
+      if (rsm) {
+        const rsmEl = xml`<set xmlns='${NS_RSM}'/>`
+        for (const key of Object.keys(rsm)) {
+          rsmEl.c(key).t(rsm[key])
+        }
+        stanza.up().cnode(rsmEl)
+      }
+
+      return this.plugins['iq-caller'].get(stanza, ...args)
+      .then(result => {
+        const rsmEl = result.getChild('set')
+        const items = result.getChild('items').children
+
+        if (rsmEl) {
+          return {
+            items,
+            rsm: rsmEl.children.reduce((obj, el) => {
+              obj[el.name] = el.text()
+              return obj
+            }, {}),
+          }
+        }
+        return {items}
+      })
+    },
+
+  }, [iqCaller, stanzaRouter])
---
+++ packages/plugins/pubsub/test.js
@@ -0,0 +1,305 @@
+'use strict'
+
+const test = require('ava')
+const plugin = require('.')
+const xml = require('@xmpp/xml')
+const testPlugin = require('../testPlugin')
+
+test.beforeEach(t => {
+  t.context = testPlugin(plugin('pubsub.foo'))
+})
+
+test('name', t => {
+  t.is(plugin().name, 'pubsub')
+})
+
+test.cb('createNode', t => {
+  t.plan(5)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'set')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const create = pubsub.getChild('create')
+    t.is(create.attrs.node, 'foo')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+          <create node='foo'/>
+        </pubsub>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.createNode('foo')
+  .then(nodeId => {
+    t.is(nodeId, 'foo')
+    t.end()
+  })
+})
+
+test.cb('createNode with config options', t => {
+  t.plan(17)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'set')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const create = pubsub.getChild('create')
+    t.is(create.attrs.node, 'foo')
+    const configure = pubsub.getChild('configure')
+    t.is(configure.name, 'configure')
+    const x = configure.getChild('x')
+    t.is(x.attrs.xmlns, 'jabber:x:data')
+    t.is(x.attrs.type, 'submit')
+    const [formType, accessModel, maxItems] = x.children
+    t.is(formType.name, 'field')
+    t.is(formType.attrs.var, 'FORM_TYPE')
+    t.is(formType.getChild('value').text(), 'http://jabber.org/protocol/pubsub#node_config')
+    t.is(accessModel.name, 'field')
+    t.is(accessModel.attrs.var, 'pubsub#access_model')
+    t.is(accessModel.getChild('value').text(), 'whitelist')
+    t.is(maxItems.name, 'field')
+    t.is(maxItems.attrs.var, 'pubsub#max_items')
+    t.is(maxItems.getChild('value').text(), '100')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+          <create node='foo'/>
+        </pubsub>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.createNode(
+    'foo',
+    {'pubsub#access_model': 'whitelist', 'pubsub#max_items': 100})
+  .then(nodeId => {
+    t.is(nodeId, 'foo')
+    t.end()
+  })
+})
+
+test.cb('deleteNode', t => {
+  t.plan(4)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'set')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const del = pubsub.getChild('delete')
+    t.is(del.attrs.node, 'foo')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.deleteNode('foo')
+  .then(t.end)
+})
+
+test.cb('publish', t => {
+  t.plan(6)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'set')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const publish = pubsub.getChild('publish')
+    t.is(publish.attrs.node, 'foo')
+    const item = publish.getChild('item')
+    const entry = item.getChild('entry')
+    t.is(entry.name, 'entry')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+          <publish node='foo'>
+            <item id='foobar'/>
+          </publish>
+        </pubsub>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.publish('foo', xml`<item><entry><title>FooBar</title></entry></item>`)
+  .then(itemId => {
+    t.is(itemId, 'foobar')
+    t.end()
+  })
+})
+
+test.cb('items', t => {
+  t.plan(11)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'get')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const items = pubsub.getChild('items')
+    t.is(items.attrs.node, 'foo')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+          <items node='foo'>
+            <item id='fooitem'>
+              <entry>Foo</entry>
+            </item>
+            <item id='baritem'>
+              <entry>Bar</entry>
+            </item>
+          </items>
+        </pubsub>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.items('foo')
+  .then(res => {
+    t.is(res.items.length, 2)
+    t.is(res.items[0].name, 'item')
+    t.is(res.items[0].attrs.id, 'fooitem')
+    t.is(res.items[0].getChildText('entry'), 'Foo')
+    t.is(res.items[1].name, 'item')
+    t.is(res.items[1].attrs.id, 'baritem')
+    t.is(res.items[1].getChildText('entry'), 'Bar')
+    t.end()
+  })
+})
+
+test.cb('items with RSM', t => {
+  t.plan(17)
+
+  t.context.entity.promise('send').then(stanza => {
+    t.is(stanza.name, 'iq')
+    t.is(stanza.attrs.type, 'get')
+    const pubsub = stanza.getChild('pubsub')
+    t.is(pubsub.attrs.xmlns, 'http://jabber.org/protocol/pubsub')
+    const items = pubsub.getChild('items')
+    t.is(items.attrs.node, 'foo')
+    const rsm = pubsub.getChild('set')
+    t.is(rsm.attrs.xmlns, 'http://jabber.org/protocol/rsm')
+    t.is(rsm.getChildText('first'), 'first@time')
+    t.is(rsm.getChildText('max'), '2')
+
+    t.context.entity.emit('element', xml`
+      <iq type='result' id='${stanza.attrs.id}'>
+        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+          <items node='foo'>
+            <item id='fooitem'>
+              <entry>Foo</entry>
+            </item>
+            <item id='baritem'>
+              <entry>Bar</entry>
+            </item>
+          </items>
+          <set xmlns='http://jabber.org/protocol/rsm'>
+            <first>first@time</first>
+            <last>last@time</last>
+            <count>2</count>
+          </set>
+        </pubsub>
+      </iq>
+    `)
+  })
+
+  t.context.plugin.items('foo', {first: 'first@time', max: 2})
+  .then(res => {
+    t.is(res.items.length, 2)
+    t.is(res.items[0].name, 'item')
+    t.is(res.items[0].attrs.id, 'fooitem')
+    t.is(res.items[0].getChildText('entry'), 'Foo')
+    t.is(res.items[1].name, 'item')
+    t.is(res.items[1].attrs.id, 'baritem')
+    t.is(res.items[1].getChildText('entry'), 'Bar')
+    t.is(res.rsm.first, 'first@time')
+    t.is(res.rsm.last, 'last@time')
+    t.is(res.rsm.count, '2')
+    t.end()
+  })
+})
+
+test.cb('item-published event', t => {
+  t.plan(7)
+
+  const p1 = new Promise(resolve => {
+    t.context.entity.on('item-published', ev => {
+      t.is(ev.node, 'foo')
+      t.is(ev.id, 'fooitem')
+      t.is(ev.entry.name, 'entry')
+      t.is(ev.entry.text(), 'Foo Bar')
+      resolve()
+    })
+  })
+  const p2 = new Promise(resolve => {
+    t.context.entity.on('item-published:foo', ev => {
+      t.is(ev.id, 'fooitem')
+      t.is(ev.entry.name, 'entry')
+      t.is(ev.entry.text(), 'Foo Bar')
+      resolve()
+    })
+  })
+
+  Promise.all([p1, p2]).then(t.end())
+
+  t.context.fake`
+    <message from='pubsub.foo'>
+      <event xmlns='http://jabber.org/protocol/pubsub#event'>
+        <items node='foo'>
+          <item id='fooitem'>
+            <entry>Foo Bar</entry>
+          </item>
+        </items>
+      </event>
+    </message>
+  `.then()
+})
+
+test.cb('last-item-published event', t => {
+  t.plan(9)
+
+  const p1 = new Promise(resolve => {
+    t.context.entity.on('last-item-published', ev => {
+      t.is(ev.node, 'foo')
+      t.is(ev.id, 'fooitem')
+      t.is(ev.entry.name, 'entry')
+      t.is(ev.entry.text(), 'Foo Bar')
+      t.is(ev.stamp, '2003-12-13T23:58:37Z')
+      resolve()
+    })
+  })
+  const p2 = new Promise(resolve => {
+    t.context.entity.on('last-item-published:foo', ev => {
+      t.is(ev.id, 'fooitem')
+      t.is(ev.entry.name, 'entry')
+      t.is(ev.entry.text(), 'Foo Bar')
+      t.is(ev.stamp, '2003-12-13T23:58:37Z')
+      resolve()
+    })
+  })
+
+  Promise.all([p1, p2]).then(t.end())
+
+  t.context.fake`
+    <message from='pubsub.foo'>
+      <event xmlns='http://jabber.org/protocol/pubsub#event'>
+        <items node='foo'>
+          <item id='fooitem'>
+            <entry>Foo Bar</entry>
+          </item>
+        </items>
+      </event>
+      <delay xmlns='urn:xmpp:delay' stamp='2003-12-13T23:58:37Z'/>
+    </message>
+  `.then()
+})
