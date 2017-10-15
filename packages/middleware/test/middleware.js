d797c666536a0315f1f08c17cac2588f926c15d8
Sonny Piers <sonny@crypho.com>
2017-10-15 16:10:13 +0200

    add router tests

--- packages/middleware/test/middleware.js
+++ packages/middleware/test/middleware.js
@@ -27,6 +27,7 @@ test.cb('use', t => {
 test.cb('filter', t => {
   t.plan(4)
   const stanza = <presence />
+  /* eslint-disable array-callback-return */
   t.context.middleware.filter((ctx, next) => {
     t.true(ctx instanceof OutgoingContext)
     t.deepEqual(ctx.stanza, stanza)
@@ -34,5 +35,6 @@ test.cb('filter', t => {
     t.true(next() instanceof Promise)
     t.end()
   })
+  /* eslint-enable array-callback-return */
   t.context.fakeOutgoing(stanza)
 })
---
+++ packages/router/test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const test = require('ava')
+const {client} = require('../test')
+const router = require('./index')
+const middleware = require('../middleware')
+
+test.beforeEach(t => {
+  t.context = client()
+  t.context.middleware = middleware(t.context.entity)
+  t.context.router = router(t.context.middleware)
+})
+
+test.cb('use', t => {
+  const stanza = <presence />
+  t.context.router.use('presence', () => {
+    t.end()
+  })
+  t.context.fakeIncoming(stanza)
+})
+
+test.cb('filter', t => {
+  const stanza = (
+    <message type="normal">
+      <foobar xmlns="foo:bar" />
+    </message>
+  )
+  t.context.router.filter('message-normal/foo:bar/foobar', () => {
+    t.end()
+  })
+  t.context.fakeOutgoing(stanza)
+})
