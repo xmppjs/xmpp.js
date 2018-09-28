58ab6986a80d1be9ccc3ace47b8db275af04b8dc
Sonny Piers <sonny@fastmail.net>
2018-09-28 16:55:59 +0200

    xmpp.js: do not export anything

--- packages/xmpp.js/test.js
+++ packages/xmpp.js/test.js
@@ -1,20 +1,20 @@
 'use strict'
 
 const test = require('ava')
-const xmpp = require('.')
 
 const fs = require('fs')
 const path = require('path')
 
-test('exports packages', t => {
-  const packages = fs
-    .readdirSync(path.join(__dirname, '..'))
-    // For some reason there's a "*"" file on travis
-    .filter(p => !['*'].includes(p) && !p.includes('.'))
+const packages = fs
+  .readdirSync(path.join(__dirname, '..'))
+  // For some reason there's a "*"" file on travis
+  .filter(p => !['*'].includes(p) && !p.includes('.'))
 
-  t.is(Object.keys(xmpp).length, packages.length)
+const dependencies = Object.keys(require('./package.json').dependencies).map(
+  dep => dep.split('@xmpp/')[1]
+)
 
-  packages.forEach(pkg => {
-    t.is(xmpp[pkg], require(path.join(__dirname, '..', pkg)))
-  })
+test('depends on all other packages', t => {
+  t.is(Object.keys(dependencies).length, packages.length)
+  t.deepEqual(dependencies, packages)
 })
