b77c04070ca7b3fbe819fc0b862099f0203d1490
Sonny Piers <sonny@fastmail.net>
2020-02-14 00:17:47 +0100

    debug: fix sensitivity of <handshake/>

---
+++ packages/xml/lib/clone.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// https://github.com/xmppjs/ltx/issues/
+// FIXME
+// probably need to prefix or unprefix the name too in some cases
+
+const _clone = require('ltx/lib/clone')
+
+module.exports = function clone(element) {
+  const c = _clone(element)
+  c.attrs.xmlns = element.getNS()
+  return c
+}
---
+++ packages/xml/test/clone.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const test = require('ava')
+const xml = require('..')
+const clone = require('../lib/clone')
+
+test('adopts parent namespace', t => {
+  const el = xml('foo', {xmlns: 'bar'}, xml('bar'))
+
+  t.deepEqual(clone(el.getChild('bar')), xml('bar', {xmlns: 'bar'}))
+})
