a35234e15bf8d1d170963ace0f2be75be58a3a0e
Mike Meessen <netmikey@gmail.com>
2021-08-21 23:46:28 +0200

    fix: try to keep backward compatibility with prosody

--- packages/connection/index.js
+++ packages/connection/index.js
@@ -263,10 +263,10 @@ class Connection extends EventEmitter {
 
     this._attachParser(new this.Parser());
 
-    // Sending this 'whitespace keepalive' packet before the stream header improves
-    // compatibility with some TLSv1.3 server implementations. For more info, see:
+    // Sending this empty packet before the stream header improves compatibility
+    // with some TLSv1.3 server implementations. For more info, see:
     // https://github.com/xmppjs/xmpp.js/issues/889#issuecomment-902686879
-    await this.write(" ");
+    await this.write("");
 
     await this.write(this.header(headerElement));
     return promise(this, "open", "error", timeout);
