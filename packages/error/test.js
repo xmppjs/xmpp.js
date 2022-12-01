dcf98bfe0699a56e59be50fb57409f690d88fa2a
Sonny Piers <sonny@fastmail.net>
2022-12-01 19:25:59 +0100

    Use getChildElements to fix bugs in SASL and XMPPError

---
+++ packages/error/test.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const test = require("ava");
+const XMPPError = require(".");
+// eslint-disable-next-line node/no-extraneous-require
+const parse = require("@xmpp/xml/lib/parse.js");
+
+test("fromElement", (t) => {
+  const application_element = (
+    <escape-your-data xmlns="http://example.org/ns" />
+  );
+
+  const nonza = (
+    <stream:error>
+      <some-condition xmlns="urn:ietf:params:xml:ns:xmpp-streams" />
+      <text xmlns="urn:ietf:params:xml:ns:xmpp-streams" xml:lang="langcode">
+        foo
+      </text>
+      {application_element}
+    </stream:error>
+  );
+
+  const error = XMPPError.fromElement(nonza);
+
+  t.is(error instanceof Error, true);
+  t.is(error.name, "XMPPError");
+  t.is(error.condition, "some-condition");
+  t.is(error.text, "foo");
+  t.is(error.application, application_element);
+});
+
+test("fromElement - whitespaces", (t) => {
+  const nonza = parse(
+    `
+    <stream:error>
+      <some-condition xmlns="urn:ietf:params:xml:ns:xmpp-streams" />
+      <text xmlns="urn:ietf:params:xml:ns:xmpp-streams" xml:lang="langcode">
+        foo
+      </text>
+      <escape-your-data xmlns='http://example.org/ns'/>
+    </stream:error>
+  `.trim(),
+  );
+
+  const error = XMPPError.fromElement(nonza);
+
+  t.is(error instanceof Error, true);
+  t.is(error.name, "XMPPError");
+  t.is(error.condition, "some-condition");
+  t.is(error.text, "\n        foo\n      ");
+  t.is(
+    error.application.toString(),
+    `<escape-your-data xmlns="http://example.org/ns"/>`,
+  );
+});
