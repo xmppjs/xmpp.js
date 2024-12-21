143f22fea1da1a2e87ff31bbcedfbebc94b35a67
Sonny Piers <sonny@fastmail.net>
2024-12-21 14:26:05 +0100

    remove base-64

--- packages/base64/browser.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable n/no-unsupported-features/node-builtins */
-
-export function encode(string) {
-  return globalThis.btoa(string);
-}
-
-export function decode(string) {
-  return globalThis.atob(string);
-}
