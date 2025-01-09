4759aa11013b6a5926e7b511b8709a07f2cf07ab
Sonny Piers <sonny@fastmail.net>
2025-01-09 01:20:02 +0100

    Remove bundlesize

---
+++ test/bundlesize.js
@@ -0,0 +1,31 @@
+/* eslint-disable unicorn/no-process-exit */
+/* eslint-disable n/no-process-exit */
+
+import zlib from "node:zlib";
+import { readFile } from "node:fs/promises";
+import bytes from "bytes";
+
+import { promisify } from "node:util";
+
+const brotliCompress = promisify(zlib.brotliCompress);
+
+const path = "packages/client/dist/xmpp.min.js";
+const buffer = await readFile(path);
+const compressed = await brotliCompress(buffer);
+
+const max_size = "15 KB";
+
+console.log(`${path}:`);
+if (compressed.length > bytes(max_size)) {
+  console.log(
+    "\u001B[31m%s\u001B[0m",
+    `${bytes(compressed.length)} > ${max_size} ❌`,
+  );
+  process.exit(1);
+} else {
+  console.log(
+    "\u001B[32m%s\u001B[0m",
+    `${bytes(compressed.length)} < ${max_size} ✅`,
+  );
+  process.exit(0);
+}
