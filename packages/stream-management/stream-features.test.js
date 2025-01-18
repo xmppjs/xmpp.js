d26aae4bc1e4c7353b240debc2e0d81adc079176
Sonny Piers <sonny@fastmail.net>
2025-01-18 16:50:59 +0100

    f

--- packages/stream-management/stream-features.test.js
+++ packages/stream-management/stream-features.test.js
@@ -124,34 +124,6 @@ test("enable - failed", async () => {
   expect(entity.streamManagement.enabled).toBe(false);
 });
 
-test("stanza ack", async () => {
-  const { entity } = mockClient();
-
-  entity.streamManagement.enabled = true;
-
-  expect(entity.streamManagement.outbound).toBe(0);
-  expect(entity.streamManagement.outbound_q).toBeEmpty();
-  // expect(entity.streamManagement.enabled).toBe(true);
-
-  await entity.send(<message id="a" />);
-
-  expect(entity.streamManagement.outbound).toBe(0);
-  expect(entity.streamManagement.outbound_q).toHaveLength(1);
-
-  let acks = 0;
-  entity.streamManagement.on("ack", (stanza) => {
-    expect(stanza.attrs.id).toBe("a");
-    acks++;
-  });
-
-  entity.mockInput(<a xmlns="urn:xmpp:sm:3" h="1" />);
-  await tick();
-
-  expect(acks).toBe(1);
-  expect(entity.streamManagement.outbound).toBe(1);
-  expect(entity.streamManagement.outbound_q).toHaveLength(0);
-});
-
 test("resume - resumed", async () => {
   const { entity } = mockClient();
 
@@ -335,19 +307,4 @@ test("resume - failed with something in queue", async () => {
   expect(entity.streamManagement.enabled).toBe(false);
   expect(entity.streamManagement.outbound).toBe(0);
   expect(entity.streamManagement.outbound_q).toBeEmpty();
-});
-
-test("sends an <a/> element before closing", async () => {
-  const { entity, streamManagement } = mockClient();
-  streamManagement.enabled = true;
-  streamManagement.inbound = 42;
-  entity.status = "online";
-
-  const promise_disconnect = entity.disconnect();
-
-  expect(await entity.catchOutgoing()).toEqual(
-    <a xmlns="urn:xmpp:sm:3" h={streamManagement.inbound} />,
-  );
-
-  await promise_disconnect;
 });
