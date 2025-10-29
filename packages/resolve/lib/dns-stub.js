809f5d04f73f8fad1bc18bd02e615f58ccda8234
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-10-29 10:40:52 -0500

    Do not try to use DNS in browser
    Fixes #1093

---
+++ packages/resolve/lib/dns-stub.js
@@ -0,0 +1,7 @@
+const lookup = null;
+const resolveSrv = null;
+const resolve = null;
+const lookupSrvs = null;
+const sortSrv = null;
+
+export { lookup, resolveSrv, resolve, lookupSrvs, sortSrv };
