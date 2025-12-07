7d2425345274c649886df7c33a3ef751ecbd7071
Sonny Piers <sonny@fastmail.net>
2025-12-07 17:46:29 +0100

    events: Remove Deferred

--- packages/events/index.js
+++ packages/events/index.js
@@ -5,7 +5,6 @@ import timeout from "./lib/timeout.js";
 import delay from "./lib/delay.js";
 import TimeoutError from "./lib/TimeoutError.js";
 import promise from "./lib/promise.js";
-import Deferred from "./lib/Deferred.js";
 import procedure from "./lib/procedure.js";
 import listeners from "./lib/listeners.js";
 import onoff from "./lib/onoff.js";
@@ -22,7 +21,6 @@ export {
   delay,
   TimeoutError,
   promise,
-  Deferred,
   procedure,
   listeners,
   onoff,
--- packages/events/lib/Deferred.js
+++
@@ -1,6 +0,0 @@
-export default function Deferred() {
-  this.promise = new Promise((resolve, reject) => {
-    this.resolve = resolve;
-    this.reject = reject;
-  });
-}
